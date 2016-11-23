8d2e94171bb99f8412bfa11bb11cc1a0a7d131fd
olegrybtsov <oleg.rybtsov@gmail.com>
2016-11-24 00:07:51 +0200

    ec

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+        access_key = "AKIAJZTCZO5P3PU7OY2A"
+        secret_key = "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"
+        region = "eu-central-1"
+}
+
+resource "aws_instance" "RH" {
+        ami = "ami-e4c63e8b"
+        instance_type = "t2.micro"
+        tags {
+                Name = "RH"
+        }
+        key_name = "mykey"
+        security_groups = ["launch-wizard-1"]
+}
