5be1d3fe57a6ada6df69824fbdb83c6f8cce81cb
olegrybtsov <oleg.rybtsov@gmail.com>
2016-11-24 19:44:49 +0200

    m

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "${var.aws_region}"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = "AKIAJZTCZO5P3PU7OY2A"
+  secret_key = "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"
 }
